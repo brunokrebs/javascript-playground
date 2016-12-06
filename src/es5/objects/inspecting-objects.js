7d8961d31e826459796396377678cf624a9c8aed
Bruno Krebs <krebs.bruno@gmail.com>
2016-12-06 10:24:43 -0200

    Playing with objects

---
+++ src/es5/objects/inspecting-objects.js
@@ -0,0 +1,57 @@
+function logProps(object, label) {
+    console.log('----------------- ' + label);
+    let empty = true;
+    for (let property in object) {
+        empty = false;
+        if (object.hasOwnProperty(property)) {
+            console.log(property);
+        } else {
+            console.log(`${property} (inherited)`);
+        }
+    }
+    if (empty) {
+        console.log('- Has no props');
+    }
+}
+
+// ****************************************************************
+// objects, inheriting directly from Object, have, by default, no properties
+logProps(Object, 'Object');
+
+// ****************************************************************
+// what if I create my own object
+
+let parent = {
+    prop1: 'prop1',
+    prop2: 2,
+    method1: function() { return 'Method 1' }
+};
+logProps(parent, 'parent');
+
+// ****************************************************************
+// now lets create a new empty object and then extend it from myObj
+
+let child = { };
+logProps(child, 'child'); // this outputs nothing
+
+Object.setPrototypeOf(child, parent);
+logProps(child, 'child'); // this outputs parent's props
+
+// ****************************************************************
+// 'third in the line' object
+
+let grandChild = { };
+logProps(grandChild, 'grandChild'); // this outputs nothing
+
+Object.setPrototypeOf(grandChild, child);
+logProps(grandChild, 'grandChild'); // this outputs parent's props
+
+// ****************************************************************
+// lets make this grandChild a composite object
+
+grandChild.backpack = {
+    money: 2,
+    scissors: 1
+};
+
+logProps(grandChild, 'grandChild'); // now with backpack, but without it's contents
